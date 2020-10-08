55e249cc3621906b6eb58d7d0825a76ca68af618
QingQing Dou <qqdou@thoughtworks.com>
2020-10-08 12:28:38 +0800

    init project
    Signed-off-by: QingQing Dou <qqdou@thoughtworks.com>

---
+++ src/main/java/com/tw/practise/TestDoublePractiseApplication.java
@@ -0,0 +1,13 @@
+package com.tw.practise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDoublePractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDoublePractiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tw/practise/TestDoublePractiseApplicationTests.java
@@ -0,0 +1,15 @@
+package com.tw.practise;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDoublePractiseApplicationTests {
+
+    @Test
+    public void should_be_correct() {
+        Assertions.assertEquals(1, 1);
+    }
+
+}
